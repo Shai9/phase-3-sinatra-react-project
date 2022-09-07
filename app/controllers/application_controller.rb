3792cfa8dc11fbb15cabc6e6ebd9c8ddab41c50f
shai9 <dennis.shakava@student.moringaschool.com>
2022-09-07 08:45:16 +0300

    implemented DELETE

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -56,7 +56,64 @@ class ApplicationController < Sinatra::Base
     activities = Activity.all
     activities.to_json
   end
+  delete '/activities/:id' do
+    activity = Activity.find(params[:id])
+    activity.costs.destroy_all
 
+    start = activity.order - 1
+    Activity.all.sort_order[start..].each do |act|
+      act.update(order: act.order-1)
+    end
+    activity.destroy
 
+    res = {'activities': Activity.all, 'costs': Cost.all}
+    res.to_json
+  end
 
+  #employees routes
+  get "/employees" do
+    employees = Employee.all
+    employees.to_json
+  end
+
+  get "/employees/:id" do
+    employees = Employee.find(params[:id])
+    employees.to_json
+  end
+
+  #costs routes
+  get "/costs" do
+    costs = Cost.all
+    costs.to_json
+  end
+
+  get '/costs/:id' do
+    costs = Cost.find(params[:id])
+    costs.to_json
+  end
+
+  post '/costs' do
+    cost = Cost.create(
+      name: params[:name],
+      total_cost: params[:total_cost],
+      category: params[:category],
+      activity_id: params[:activity_id],
+      employee_id: params[:employee_id]
+    )
+    cost.to_json
+  end
+
+  patch '/costs/:id' do
+    cost = Cost.find(params[:id])
+    cost.update(
+      total_cost: params[:total_cost]
+    )
+    cost.to_json
+  end
+
+  delete '/costs/:id' do
+    cost = Cost.find(params[:id])
+    cost.destroy
+    cost.to_json
+  end
 end
