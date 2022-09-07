45c9021dd3b9e312d807690b3d2000464c1fcc60
shai9 <dennis.shakava@student.moringaschool.com>
2022-09-07 08:38:33 +0300

    implemented GET requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,36 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+ 
+  get '/' do
+    "Hello Moringa"
   end
+  
+  #activities routes 
+  get "/activities" do
+    activities = Activity.all
+    activities.to_json
+  end
+
+  get "/activities/:id" do
+    activities = Activity.find(params[:id])
+    activities.to_json
+  end
+
+  get "/activities/:id/costs" do
+    costs = Activity.find(params[:id]).costs
+    costs.to_json
+  end
+
+  get "/project_cost" do
+    cost = [Activity.project_cost]
+    cost.to_json
+  end
+
+  get "/project_hours" do 
+    hours = [Activity.project_hours]
+    hours.to_json
+  end
+
 
 end
